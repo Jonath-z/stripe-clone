ec205b401ab433dffd30ed2be9baed9f132994d9
Jonath-z <jonathanzihindula95@gmail.com>
2022-10-19 15:30:18 +0200

    deskversion improved

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,8 @@ module.exports = {
         "semi-gray": "#0a2540",
         "stripe-blue": "#635bff",
         "stripe-sky-blue": "#00d4ff",
+        "stripe-deco-blue": "#02bcf5",
+        "stripe-semi-violet": "#7a73ff",
       },
     },
   },
